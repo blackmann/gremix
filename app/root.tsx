c453576f8ab3062a1aeb9e93693d450b998140d0
Degreat <mail@degreat.co.uk>
2024-06-11 08:16:13 +0000

    use tailwind compat

--- app/root.tsx
+++ app/root.tsx
@@ -1,7 +1,6 @@
-import 'virtual:uno.css';
-
-import '@unocss/reset/tailwind.css';
-import './styles.css';
+import "@unocss/reset/tailwind-compat.css";
+import "virtual:uno.css";
+import "./styles.css";
 
 import {
   Links,
@@ -10,27 +9,27 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import { PendingUI } from './components/pending-ui';
+import { PendingUI } from "./components/pending-ui";
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <PendingUI />
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				<PendingUI />
+				{children}
+				<ScrollRestoration />
+				<Scripts />
+			</body>
+		</html>
+	);
 }
 
 export default function App() {
-  return <Outlet />;
+	return <Outlet />;
 }
