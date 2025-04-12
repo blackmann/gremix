4c4a2db8b536ae6792c51f454bfabe881d8f7177
De-Great Yartey <mail@degreat.co.uk>
2025-04-12 13:31:14 +0000

    update deps

--- uno.config.ts
+++ uno.config.ts
@@ -2,7 +2,7 @@ import { presetForms } from "@julr/unocss-preset-forms";
 import {
 	defineConfig,
 	presetIcons,
-	presetWind,
+	presetWind3,
 	transformerDirectives,
 	transformerVariantGroup,
 } from "unocss";
@@ -11,6 +11,6 @@ export default defineConfig({
 	content: {
 		filesystem: ["**/*.{html,js,ts,jsx,tsx,vue,svelte,astro}"],
 	},
-	presets: [presetWind({ dark: "media" }), presetIcons(), presetForms()],
+	presets: [presetWind3({ dark: "media" }), presetIcons(), presetForms()],
 	transformers: [transformerDirectives(), transformerVariantGroup()],
 });
