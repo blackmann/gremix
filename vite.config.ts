3be221098dba92d8deac8ec713f30152b63d0c3f
De-Great Yartey <mail@degreat.co.uk>
2025-07-07 00:30:09 +0000

    exclude uno css

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [reactRouterHonoServer(), reactRouter(), UnoCSS(), tsconfigPaths()],
+	plugins: [
+		reactRouterHonoServer({ dev: { exclude: ["/__uno.css"] } }),
+		reactRouter(),
+		UnoCSS(),
+		tsconfigPaths(),
+	],
 });
